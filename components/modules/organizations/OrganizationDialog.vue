89eb210b0297990a60b353097aa0685695888b4c
rdiazutn <rodrigodiaz@frba.utn.edu.ar>
2022-06-29 20:51:58 -0300

    Se agrega sh async dialog

---
+++ components/modules/organizations/OrganizationDialog.vue
@@ -0,0 +1,49 @@
+<template>
+  <ShAsyncDialog
+    width="500"
+    confirm-text="Crear"
+    title="Crear organización"
+    :async-confirm-function="save"
+  >
+    <template #activator="{on}">
+      <ShButton :block="$vuetify.breakpoint.smAndDown" v-on="on">
+        <v-icon color="white">
+          mdi-plus
+        </v-icon>
+        Crear organización
+      </ShButton>
+    </template>
+    <template #default>
+      <div>
+        <ShTextField
+          v-model="organization.name"
+          label="Nombre *"
+          :rules="[$rules.required('nombre')]"
+        />
+      </div>
+      <div>
+        <ShTextField
+          v-model="organization.color"
+          label="Color *"
+          :rules="[$rules.required('nombre')]"
+        />
+      </div>
+    </template>
+  </ShAsyncDialog>
+</template>
+<script>
+export default {
+  data: () => ({
+    organization: {
+      name: '',
+      color: ''
+    }
+  }),
+  methods: {
+    save () {
+      console.log('SAVE')
+      return Promise.resolve(true)
+    }
+  }
+}
+</script>
