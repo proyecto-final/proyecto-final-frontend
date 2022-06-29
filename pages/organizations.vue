6eebe9bcbc6f9d94255cc7ed98a8fc1416f92bcb
rdiazutn <rodrigodiaz@frba.utn.edu.ar>
2022-06-29 18:50:31 -0300

    Crud de organización vista

---
+++ pages/organizations.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="d-flex justify-space-between">
+      <div>
+        <ShSearchField placeholder="Buscar por nombre" />
+      </div>
+      <div>
+        <ShButton>
+          <v-icon color="white">
+            mdi-plus
+          </v-icon>
+          Crear organización
+        </ShButton>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  created () {
+    this.$store.commit('navigation/SET_PAGE_TITLE', 'Organizaciones')
+  }
+}
+</script>
