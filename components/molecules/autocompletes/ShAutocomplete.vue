f092219cde0697836cae02af8bd7a37d9ff4db4b
Rodrigo Diaz <43448566+rdiazutn@users.noreply.github.com>
2022-07-05 00:41:25 -0300

    Revert "Revert "Feature/sc 58/crud de usuarios vista específica perfil""

--- components/molecules/autocompletes/ShAutocomplete.vue
+++ components/molecules/autocompletes/ShAutocomplete.vue
@@ -28,7 +28,7 @@ export default {
 </script>
 <style scoped>
 :not(.v-input--is-focused) ::v-deep .v-input__slot fieldset {
-  border-color: var(--v-neutral-lighten2);
+  border-color: var(--v-neutral-darken1);
 }
 :not(.v-input--is-focused) ::v-deep .v-icon {
   color: var(--v-neutral-darken2) !important;
