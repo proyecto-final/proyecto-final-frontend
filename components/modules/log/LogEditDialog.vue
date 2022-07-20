8d19c4adc17e88e4d01d69a8d72a8b14dfad4ed2
juancueli99 <juanfcue99@gmail.com>
2022-07-19 21:43:55 -0300

    Subo el dialog para editar el nombre y descripción de un log.

---
+++ components/modules/log/LogEditDialog.vue
@@ -0,0 +1,75 @@
+<template>
+  <ShAsyncDialog
+    width="500"
+    confirm-text="Guardar"
+    title="Editar log"
+    :async-confirm-function="save"
+    v-on="$listeners"
+    @open="editLog"
+  >
+    <template #activator="{on}">
+      <slot name="activator" :on="on">
+        <v-list-item
+          v-on="on"
+        >
+          <ShBody>
+            Editar
+          </ShBody>
+        </v-list-item>
+      </slot>
+    </template>
+    <template #default>
+      <div>
+        <ShTextField
+          :key="index"
+          v-model="log.name"
+          label="Nombre *"
+          :rules="[$rules.required('nombre')]"
+          class="mr-4 mt-4"
+        />
+      </div>
+      <div>
+        <ShTextArea
+          :key="index"
+          v-model="log.description"
+          label="Descripción"
+          class="mr-4"
+        />
+      </div>
+    </template>
+  </ShAsyncDialog>
+</template>
+<script>
+import { cloneDeep } from 'lodash'
+const getEmptyLog = () => ({
+  name: '',
+  description: ''
+})
+export default {
+  props: {
+    log2Edit: {
+      type: Object,
+      default: null
+    },
+    organizationId: {
+      type: String,
+      required: true
+    },
+    projectId: {
+      type: Number,
+      default: null
+    }
+  },
+  data: () => ({
+    log: getEmptyLog()
+  }),
+  methods: {
+    save () {
+      // To-Do
+    },
+    editLog () {
+      this.log = cloneDeep(this.log2Edit)
+    }
+  }
+}
+</script>
--- pages/logs.vue
+++ pages/logs.vue
@@ -117,6 +117,14 @@
               <v-list>
                 Eliminar
               </v-list>
+              <v-list>
+                <LogEditDialog
+                  :organization-id="organizationId"
+                  :project-id="item.id"
+                  :log2-edit="item"
+                  @updated="$fetch"
+                />
+              </v-list>
             </v-menu>
           </div>
         </template>
@@ -167,7 +175,7 @@ export default {
       limit: this.options.itemsPerPage,
       ...this.filter
     }).then((result) => {
-      this.logs = [{ name: 'Logcito 1', description: 'Desc log 1', date: '2022-07-17 22:21:58', status: 'loaded' }, { name: 'Logcito 2', description: 'Desc log 2', date: '2022-07-17 22:21:58', status: 'loading' }]
+      this.logs = [{ id: '1', name: 'Logcito 1', description: 'Desc log 1', date: '2022-07-17 22:21:58', status: 'loaded' }, { id: '2', name: 'Logcito 2', description: 'Desc log 2', date: '2022-07-17 22:21:58', status: 'loading' }]
       this.serverItemsLength = 2
     }).catch(() => {
       this.$noty.warn('Hubo un error al cargar las organizaciones')
