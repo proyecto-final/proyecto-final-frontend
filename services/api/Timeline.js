197029b5ee036f00805a375f62c58f2b74ced93d
EzeDiaz <ezequiel.dz@gmail.com>
2022-07-27 21:51:53 -0300

    Subo vista de timelines (WIP - Back mockeado)

---
+++ services/api/Timeline.js
@@ -0,0 +1,11 @@
+export default class Timeline {
+  constructor ({ $axios, store, router }) {
+    this.$axios = $axios
+    this.store = store
+    this.router = router
+  }
+
+  get (projectId, params) {
+    return Promise.resolve([{ _id: '1', title: 'Timeline 1', description: 'Un timeline', updatedAt: '2022-07-27T23:35:26.786Z', eventsCount: '123' }, { _id: '2', title: 'Timeline 2', description: 'Otro timeline', updatedAt: '2022-07-27T23:35:26.786Z', eventsCount: '567' }])
+  }
+}
