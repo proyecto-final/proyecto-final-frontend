60fc5a74cccb40e0006a165be4de4b27a36095da
tinchourteaga <38136900+tinchourteaga@users.noreply.github.com>
2022-07-29 17:36:48 -0300

    log line view row

--- pages/_projectId/logs/_logId.vue
+++ pages/_projectId/logs/_logId.vue
@@ -1,6 +1,53 @@
 <template>
   <div>
-    pepe
+    <div>
+      <v-row justify="space-between">
+        <v-col cols="12" md="4" lg="3">
+          <ShSearchField
+            v-model="keyword"
+            hide-details
+            clearable
+            placeholder="Buscar por palabra clave"
+            maxlength="32"
+          />
+        </v-col>
+        <v-col cols="12" md="4" lg="3">
+          <div class="d-flex justify-end">
+            <ShButton>
+              Previsualizar timeline
+            </ShButton>
+          </div>
+        </v-col>
+      </v-row>
+      <v-row class="mb-6">
+        <v-col cols="12" md="4" lg="3">
+          <ShAutocomplete
+            v-model="state"
+            hide-details
+            clearable
+            :items="[{ text: '24/02/2022', value: 'processed' }, { text: '05/11/2021', value: 'processing' }]"
+            placeholder="Filtrar por fecha"
+          />
+        </v-col>
+        <v-col cols="12" md="4" lg="3">
+          <ShAutocomplete
+            v-model="eventId"
+            hide-details
+            clearable
+            :items="[{ text: '4624', value: '4624' }, { text: '24', value: '24' }]"
+            placeholder="Filtrar por evento"
+          />
+        </v-col>
+      </v-row>
+      <v-row style="height:800px">
+        <div v-for="(line, index) in lines" :key="index" class="log-div">
+          {{ index + 1 }} - {{ line.raw }}
+        </div>
+        <div class="timeline-div">
+          HOLA
+        </div>
+      </v-row>
+    </div>
   </div>
 </template>
 <script>
@@ -22,3 +69,25 @@ export default {
   }
 }
 </script>
+<style scoped>
+.log-div {
+  background-color: white;
+  border-top-style: solid !important;
+  border-top-color: var(--v-background-base) !important;
+  border-top: 1px;
+  width: 80%;
+  margin-left: -10px;
+  height: 100%;
+}
+.timeline-div {
+  background-color: white;
+  border-top-style: solid !important;
+  border-top-color: var(--v-background-base) !important;
+  border-left-style: solid !important;
+  border-left-color: var(--v-background-base) !important;
+  border-top: 1px;
+  border-left: 1px;
+  width: 20%;
+  height: 100%;
+}
+</style>
