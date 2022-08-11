953785c9ada509c6884b8857b79ea45eccb217c4
juancueli99 <juanfcue99@gmail.com>
2022-08-10 22:09:18 -0300

    Ahora aparece el empty state

--- pages/_projectId/timelines.vue
+++ pages/_projectId/timelines.vue
@@ -24,7 +24,7 @@
             clearable
             placeholder="Buscar por nombre"
             maxlength="32"
-            @input="fetchDebounced"
+            @input="search"
           />
         </v-col>
       </v-row>
