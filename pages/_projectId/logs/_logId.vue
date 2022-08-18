98e28ebc53cf36c2a707eb5f4a64c27f170dfb0d
EzeDiaz <ezequiel.dz@gmail.com>
2022-08-18 13:00:05 -0300

    Hago uso de assign como sugiere el PR

--- pages/_projectId/logs/_logId.vue
+++ pages/_projectId/logs/_logId.vue
@@ -249,7 +249,10 @@ export default {
     },
     setLine (line, updatedLine) {
       Object.assign(line, updatedLine)
-      extend(this.timelineLines.find(timelineLine => timelineLine._id === updatedLine._id), updatedLine)
+      const timelineLineToUpdate = this.timelineLines.find(timelineLine => timelineLine._id === updatedLine._id)
+      if (timelineLineToUpdate) {
+        Object.assign(timelineLineToUpdate, updatedLine)
+      }
     },
     getNextLines (entries) {
       if (entries[0].isIntersecting && !this.loading) {
