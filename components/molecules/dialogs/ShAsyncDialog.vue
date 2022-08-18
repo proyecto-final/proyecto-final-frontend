8b3c70f710522e317946e0f0ea5a0287b62fe0bb
Martín Urteaga Naya <38136900+tinchourteaga@users.noreply.github.com>
2022-08-18 13:18:24 -0300

    Feature/sc 355/agregar notas frontend (#57)
    * First approach note
    
    * Fucking css
    
    * Customizing notes
    
    * no title and fixed css
    
    * Behavior
    
    * Habemus scrollbar
    
    * Delete note and char limit
    
    * Brace yourselves changes are coming
    
    * Selected note is editable
    
    * Fixes in v-model and methods
    
    * Save notes
    
    * FINISH HIM
    
    * Lines with note chips
    
    * css fixes
    
    * fix condition
    
    * fix bugs
    
    * fix conditional
    
    * css fix
    
    * fix rule
    
    * v-alert and removed conditions
    
    * save method minor fix
    
    * Fixes conditional and css
    
    * remove condition

--- components/molecules/dialogs/ShAsyncDialog.vue
+++ components/molecules/dialogs/ShAsyncDialog.vue
@@ -113,6 +113,10 @@ export default {
     submitOnEnter: {
       type: Boolean,
       default: true
+    },
+    isNote: {
+      type: Boolean,
+      default: false
     }
   },
   data: () => ({
