d1a6098d8f7bf2e3b8962cf96c6e61d537871d00
tinchourteaga <38136900+tinchourteaga@users.noreply.github.com>
2022-08-20 14:07:13 -0300

    All charts in timelineCharts

--- components/modules/timeline/TimelineCharts.vue
+++ components/modules/timeline/TimelineCharts.vue
@@ -3,13 +3,13 @@
     <v-col class="d-flex justify-center">
       <ShChartCard
         title="Eventos detectados"
-        description="Cantidad de eventos independientes detectados en el log"
+        description="Cantidad de eventos independientes detectados en la evidencia analizada. Dichos eventos representan las acciones de los usuarios dentro de los equipos involucrados en la investigación."
       >
         <ShVerticalBarChart
           :chart-data="{
             labels: ['Jan', 'Feb', 'Mar'],
             datasets: [{
-              label: 'Login lperez',
+              label: '',
               backgroundColor: 'rgba(54, 162, 235, 0.2)',
               borderWidth: 1,
               borderColor: 'rgb(54, 162, 235)',
@@ -21,8 +21,8 @@
     </v-col>
     <v-col class="d-flex justify-center">
       <ShChartCard
-        title="Login de usuarios"
-        description="Intentos de login de usuarios sospechosos"
+        title="Vulnerabilidades detectada"
+        description="Potenciales intentos de explotación de vulnerabilidades asociadas a distintos patrones de ataque reconocidos por la plataforma de inteligencia MITRE ATT&CK."
       >
         <ShHorizontalBarChart
           :chart-data="{
@@ -32,6 +32,84 @@
               backgroundColor: 'rgba(54, 162, 235, 0.2)',
               borderWidth: 1,
               borderColor: 'rgb(54, 162, 235)',
+              data: [40,20,30]
+            }]
+          }"
+        />
+      </ShChartCard>
+    </v-col>
+    <v-col class="d-flex justify-center">
+      <ShChartCard
+        title="Vulnerabilidades detectada"
+        description="Potenciales intentos de explotación de vulnerabilidades conocidas y asociadas a distintos patrones de ataque reconocidos por la plataforma de inteligencia MITRE ATT&CK"
+      >
+        <ShPieChart
+          :chart-data="{
+            labels: ['plopez', 'mrey', 'jmiranda'],
+            datasets: [{
+              backgroundColor: 'rgba(54, 162, 235, 0.2)',
+              borderWidth: 1,
+              borderColor: 'rgb(54, 162, 235)',
+              data: [40,20,30]
+            }]
+          }"
+        />
+      </ShChartCard>
+    </v-col>
+    <v-col class="d-flex justify-center">
+      <ShChartCard
+        title="Vulnerabilidades detectada"
+        description="Potenciales intentos de explotación de vulnerabilidades asociadas a distintos patrones de ataque reconocidos por la plataforma de inteligencia MITRE ATT&CK"
+      >
+        <ShDoughnutChart
+          :chart-data="{
+            labels: ['plopez', 'mrey', 'jmiranda'],
+            datasets: [{
+              backgroundColor: 'rgba(54, 162, 235, 0.2)',
+              borderWidth: 1,
+              borderColor: 'rgb(54, 162, 235)',
+              data: [40,20,30]
+            }]
+          }"
+        />
+      </ShChartCard>
+    </v-col>
+    <v-col class="d-flex justify-center">
+      <ShChartCard
+        title="Vulnerabilidades detectada"
+        description="Potenciales intentos de explotación de vulnerabilidades asociadas a distintos patrones de ataque reconocidos por la plataforma de inteligencia MITRE ATT&CK"
+      >
+        <ShLineChart
+          :chart-data="{
+            labels: ['plopez', 'mrey', 'jmiranda'],
+            datasets: [{
+              backgroundColor: 'rgba(54, 162, 235, 0.2)',
+              borderWidth: 1,
+              borderColor: 'rgb(54, 162, 235)',
+              pointBackgroundColor: 'blue',
+              pointBorderColor: 'blue',
+              tension: 0.1,
+              data: [40,20,30]
+            }]
+          }"
+        />
+      </ShChartCard>
+    </v-col>
+    <v-col class="d-flex justify-center">
+      <ShChartCard
+        title="Vulnerabilidades detectada"
+        description="Potenciales intentos de explotación de vulnerabilidades asociadas a distintos patrones de ataque reconocidos por la plataforma de inteligencia MITRE ATT&CK"
+      >
+        <ShAreaChart
+          :chart-data="{
+            labels: ['plopez', 'mrey', 'jmiranda'],
+            datasets: [{
+              backgroundColor: 'rgba(54, 162, 235, 0.2)',
+              borderWidth: 1,
+              borderColor: 'rgb(54, 162, 235)',
+              pointBackgroundColor: 'blue',
+              pointBorderColor: 'blue',
+              tension: 0.1,
               data: [40,20,30]
             }]
           }"
--- components/molecules/charts/ShDoughnutChart.vue
+++ components/molecules/charts/ShDoughnutChart.vue
@@ -7,9 +7,12 @@
   />
 </template>
 <script>
+import { Doughnut } from 'vue-chartjs/legacy'
 import pieChartMixin from '@/services/helpers/mixins/pieChartMixin'
 
 export default {
+  name: 'DoughnutChart',
+  components: { Doughnut },
   mixins: [pieChartMixin],
   computed: {
     chartOptionsToShow () {
