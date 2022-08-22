c066b4e37d08c36c049aa1298d67ae39960cc173
juancueli99 <juanfcue99@gmail.com>
2022-08-22 00:01:29 -0300

    IP view has being desingned

---
+++ components/modules/search-ip/SearchIpCard.vue
@@ -0,0 +1,132 @@
+<template>
+  <v-card
+    :color="!ip.isReported ? '#E4F5E5' : '#FFF0EC'"
+    flat
+    class="ml-4 mt-4 w-30"
+  >
+    <v-card-text class="pa-6">
+      <ShBodySmall neutral class="mb-2">
+        La reputación de la IP es
+        <ShBodySmall strong :class="`${!ip.isReported ? 'green-text' : 'red-text'}`">
+          {{ !ip.isReported ? '0%' : `${ip.reputation}%` }}
+        </ShBodySmall>
+      </ShBodySmall>
+      <ShHeading1 class="mb-4 mr-4">
+        94.222.32.14
+        <ShBodySmall strong :class="`${!ip.isReported ? 'green-text' : 'red-text'}`">
+          {{ !ip.isReported ? 'Sin Reportes' : 'Reportada' }}
+        </ShBodySmall>
+      </ShHeading1>
+      <v-row class="my-4 ml-1">
+        <v-col>
+          <v-row>
+            <ShBodySmall neutral>
+              País
+            </ShBodySmall>
+          </v-row>
+          <v-row>
+            <ShBodySmall neutral strong>
+              {{ ip.country }}
+            </ShBodySmall>
+          </v-row>
+        </v-col>
+        <v-col>
+          <v-row>
+            <ShBodySmall neutral>
+              Ciudad
+            </ShBodySmall>
+          </v-row>
+          <v-row>
+            <ShBodySmall neutral strong>
+              {{ ip.city }}
+            </ShBodySmall>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-divider class="my-4" />
+      <v-row class="my-4 ml-1">
+        <v-col>
+          <v-row>
+            <ShBodySmall neutral>
+              ISP
+            </ShBodySmall>
+          </v-row>
+          <v-row>
+            <ShBodySmall neutral strong>
+              {{ ip.isp }}
+            </ShBodySmall>
+          </v-row>
+        </v-col>
+        <v-col>
+          <v-row>
+            <ShBodySmall neutral>
+              ASN
+            </ShBodySmall>
+          </v-row>
+          <v-row>
+            <ShBodySmall neutral strong>
+              {{ ip.asn }}
+            </ShBodySmall>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row class="mt-4 ml-1">
+        <v-col>
+          <v-row>
+            <ShBodySmall neutral>
+              TOR
+            </ShBodySmall>
+          </v-row>
+          <v-row>
+            <ShBodySmall neutral strong>
+              {{ ip.tor ? 'Si' : 'No' }}
+            </ShBodySmall>
+          </v-row>
+        </v-col>
+        <v-col>
+          <v-row>
+            <ShBodySmall neutral>
+              VPN
+            </ShBodySmall>
+          </v-row>
+          <v-row>
+            <ShBodySmall neutral strong>
+              {{ ip.vpn ? 'Si' : 'No' }}
+            </ShBodySmall>
+          </v-row>
+        </v-col>
+      </v-row>
+      <template v-if="ip.isReported">
+        <v-divider class="mt-8 mb-4" />
+        <div>
+          <ShBodySmall neutral strong class="ml-1">
+            {{ `Reportes (${ip.reports})` }}
+          </ShBodySmall>
+          <v-icon color="neutral base" class="float-right">
+            mdi-chevron-down
+          </v-icon>
+        </div>
+      </template>
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    ip: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+<style scoped>
+.green-text{
+  color: #086E08;
+  vertical-align: middle
+}
+.red-text{
+  color: #E3231D;
+  vertical-align: middle
+}
+</style>
--- pages/_projectId/search-ip.vue
+++ pages/_projectId/search-ip.vue
@@ -1,23 +1,57 @@
 <template>
-  <div>
-    <v-card class="d-flex align-center justify-space-between pa-4">
-      <div>
-        <v-card-title>Página en construcción</v-card-title>
-        <v-card-subtitle>Será desarrollada a la brevedad</v-card-subtitle>
-      </div>
-      <div>
-        <v-icon large>
-          mdi-account-hard-hat
-        </v-icon>
-      </div>
-    </v-card>
+  <div class="mb-6">
+    <v-row justify="space-between">
+      <v-col cols="12" md="4" lg="3">
+        <ShSearchField
+          v-model="filter.name"
+          hide-details
+          clearable
+          placeholder="Buscar IP"
+          maxlength="32"
+          class="ml-4 mt-4"
+          @input="search"
+        />
+      </v-col>
+    </v-row>
+    <SearchIpCard :ip="ip" />
   </div>
 </template>
 <script>
+import { debounce } from 'lodash'
 export default {
+  data: () => ({
+    loading: false,
+    filter: {
+      name: null,
+      date: null
+    },
+    ip: {
+      isReported: true,
+      reputation: 70,
+      reports: 467,
+      country: 'Argentina',
+      city: 'General Rodríguez',
+      isp: 'Fibertel',
+      asn: 'AS3209',
+      tor: false,
+      vpn: false
+    }
+  }),
+  fetch () {
+    this.loading = true
+  },
   created () {
     this.$store.commit('navigation/SET_PAGE_TITLE', 'Buscar IPS')
     this.$store.commit('navigation/CAN_GO_BACK', false)
+  },
+  methods: {
+    search () {
+      this.loading = true
+      this.fetchDebounced()
+    },
+    fetchDebounced: debounce(function () {
+      this.$fetch()
+    }, 500)
   }
 }
 </script>
