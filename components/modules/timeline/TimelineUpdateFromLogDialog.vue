66274589440f7ed253a2a822ac8b3324153492b5
Rodrigo Diaz <43448566+rdiazutn@users.noreply.github.com>
2022-08-21 22:06:59 -0300

    feat: [sc-178] Timeline: Actualizacion(frontend) (#77)
    * New component for updating a timeline
    
    * Adaptacion de creacion y vista de timelines para que acepten un array de logids
    
    * fix tarea
    
    * Desarrollo de actualizacion
    
    * Fix pr
    
    * Cambio mensaje del dialog
    
    Co-authored-by: juancueli99 <juanfcue99@gmail.com>
    Co-authored-by: EzeDiaz <ezequiel.dz@gmail.com>

---
+++ components/modules/timeline/TimelineUpdateFromLogDialog.vue
@@ -0,0 +1,52 @@
+<template>
+  <ShAsyncDialog
+    width="600"
+    confirm-text="Actualizar"
+    title="Actualizar timeline"
+    success-text="Timeline actualizado"
+    :async-confirm-function="updateFromLog"
+    persistent
+    v-on="$listeners"
+  >
+    <template #activator="{on}">
+      <slot name="activator" :on="on">
+        <ShButton class="ma-2" text :block="$vuetify.breakpoint.smAndDown" v-on="on">
+          Actualizar
+        </ShButton>
+      </slot>
+    </template>
+    <template #default>
+      <ShHeading2 class="d-flex justify-center my-4">
+        ¿Estás seguro que deseás actualizar la timeline?
+      </ShHeading2>
+      <v-alert type="warning" icon="mdi-alert" class="justify-space-between mb-6 mt-2">
+        <ShBodySmall class="white-text">
+          Al actualizar la timeline todos los datos actuales serán reemplazados por los que se encuentran en el log asociado a la misma.
+        </ShBodySmall>
+      </v-alert>
+    </template>
+  </ShAsyncDialog>
+</template>
+<script>
+export default {
+  props: {
+    timelineId: {
+      type: String,
+      required: true
+    },
+    projectId: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    updateFromLog () {
+      return this.$timelineService.updateFromLog(this.projectId, this.timelineId)
+        .then(() => {
+          this.$emit('update')
+          return true
+        })
+    }
+  }
+}
+</script>
