dc09083aefc02dbaee12b6b44e073aa42a14e2ed
juancueli99 <juanfcue99@gmail.com>
2022-08-24 14:08:40 -0300

    First aproximation

--- components/molecules/chips/ShStatusChip.vue
+++ components/molecules/chips/ShStatusChip.vue
@@ -1,9 +1,20 @@
 <template>
-  <ShChip
-    :color="status === 'processed' ? 'success' : (status === 'processing' ? 'warning' : 'error')"
-  >
-    {{ status === 'processed' ? 'Cargado' : (status === 'processing' ? 'Procesando...' : 'Error' ) }}
-  </ShChip>
+  <div>
+    <ShChip
+      :color="status === 'processed' ? 'success' : (status === 'processing' ? 'warning' : 'error')"
+    >
+      {{ status === 'processed' ? 'Cargado' : (status === 'processing' ? 'Procesando...' : 'Error' ) }}
+    </ShChip>
+    <v-progress-linear
+      v-model="value"
+      rounded
+      class="mt-2"
+      color="primary"
+      :active="show"
+      :indeterminate="query"
+      :query="true"
+    />
+  </div>
 </template>
 <script>
 export default {
@@ -11,6 +22,38 @@ export default {
     status: {
       type: String,
       required: true
+    }
+  },
+  data () {
+    return {
+      value: 0,
+      query: false,
+      show: true,
+      interval: 0
+    }
+  },
+  mounted () {
+    this.queryAndIndeterminate()
+  },
+  beforeDestroy () {
+    clearInterval(this.interval)
+  },
+  methods: {
+    queryAndIndeterminate () {
+      this.query = true
+      this.show = true
+      this.value = 0
+      setTimeout(() => {
+        this.query = false
+        this.interval = setInterval(() => {
+          if (this.value === 100) {
+            clearInterval(this.interval)
+            this.show = false
+            return setTimeout(this.queryAndIndeterminate, 2000)
+          }
+          this.value += 25
+        }, 1000)
+      }, 2500)
     }
   }
 }
