d46563032b6058ddabd08b356ea3180e098eff19
Juan Cueli <37660905+juancueli99@users.noreply.github.com>
2022-08-26 00:49:50 -0300

    Feature/sc 152/ips vista frontend (#78)
    * IP view has being desingned
    
    * Reports are now displayed
    
    * Empty State is added
    
    * Connecting front with back
    
    * Fixes
    
    * Rules fixes
    
    * More little fixes
    
    * More little fixes
    
    * Another fix
    
    * Chevron Fix
    
    * Some fixes
    
    * Checking rules before submitting
    
    * Justify text
    
    * A computed that returns an object is created to avoid multiple ternary operators.

--- services/helpers/Rules.js
+++ services/helpers/Rules.js
@@ -7,6 +7,11 @@ export default class Rules {
     return value => (!!value && value.trim().length !== 0) || `El campo ${field} debe poseer un caracter que no sea espacio`
   }
 
+  ipFormat (value) {
+    const regexExp = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi
+    return regexExp.test(value) || 'El campo debe poseer un formato válido de IP'
+  }
+
   maxLength (max) {
     return value => value.length <= max || `El campo debe tener hasta ${max} caracteres`
   }
