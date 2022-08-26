54fde2a5b47786e40b2bfa665e2e893d2fa8fbf1
rdiazutn <rodrigodiaz@frba.utn.edu.ar>
2022-08-25 23:36:41 -0300

    fix

--- components/modules/timeline/TimelineDownloadScreenAsPdfButton.vue
+++ components/modules/timeline/TimelineDownloadScreenAsPdfButton.vue
@@ -22,25 +22,29 @@ export default {
   },
   methods: {
     async downloadPdf () {
-      // eslint-disable-next-line promise/param-names
-      const delay = ms => new Promise(res => setTimeout(res, ms))
-      const timelineId = this.timelineId
-      const element = document.getElementById('v-app-root')
-      const doc = new JsPDF('p', 'px', [element.clientHeight / 1.77, element.clientWidth / 1.77])
-      await html2canvas(element).then(function (canvas) {
-        const imgData = canvas.toDataURL('image/png')
-        doc.addImage(imgData, 'PNG', 0, 0)
-      })
-      this.$emit('switchPage', 1)
-      await delay(500)
-      const secondElement = document.getElementById('v-app-root')
-      doc.addPage('p', 'px', [secondElement.clientHeight / 1.77, secondElement.clientWidth / 1.77])
-      await html2canvas(secondElement).then(function (canvas) {
-        const imgData = canvas.toDataURL('image/png')
-        doc.addImage(imgData, 'PNG', 0, 0)
-      })
-      doc.save(`reporte-timeline-${timelineId}.pdf`)
-      await this.$emit('switchPage', 0)
+      try {
+        // eslint-disable-next-line promise/param-names
+        const delay = ms => new Promise(res => setTimeout(res, ms))
+        const timelineId = this.timelineId
+        const element = document.getElementById('v-app-root')
+        const doc = new JsPDF('p', 'px', [element.clientHeight / 1.77, element.clientWidth / 1.77])
+        await html2canvas(element).then(function (canvas) {
+          const imgData = canvas.toDataURL('image/png')
+          doc.addImage(imgData, 'PNG', 0, 0)
+        })
+        this.$emit('switchPage', 1)
+        await delay(500)
+        const secondElement = document.getElementById('v-app-root')
+        doc.addPage('p', 'px', [secondElement.clientHeight / 1.77, secondElement.clientWidth / 1.77])
+        await html2canvas(secondElement).then(function (canvas) {
+          const imgData = canvas.toDataURL('image/png')
+          doc.addImage(imgData, 'PNG', 0, 0)
+        })
+        doc.save(`reporte-timeline-${timelineId}.pdf`)
+        await this.$emit('switchPage', 0)
+      } catch {
+        this.$noty.warn('Ocurrió un error al descargar la timeline')
+      }
     }
   }
 }
