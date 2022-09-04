a6f2c5e8ff468c07d9fc0d5de4e82cb842a24b88
juancueli99 <juanfcue99@gmail.com>
2022-09-03 21:04:00 -0300

    Charts are added

---
+++ components/molecules/charts/ShLineChart.vue
@@ -0,0 +1,14 @@
+<template>
+  <ChartLine
+    :chart-options="chartOptions"
+    :chart-data="chartDataWithColors"
+    :width="width"
+    :height="height"
+  />
+</template>
+<script>
+import lineChartMixin from '@/services/helpers/mixins/lineChartMixin'
+export default {
+  mixins: [lineChartMixin]
+}
+</script>
