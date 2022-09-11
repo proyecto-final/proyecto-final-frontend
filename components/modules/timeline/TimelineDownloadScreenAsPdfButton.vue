b55ad57b8aa02c7c5091c8266a60438d6e83cdbe
rdiazutn <rodrigodiaz@frba.utn.edu.ar>
2022-09-11 11:36:03 -0300

    Fix pr

--- components/modules/timeline/TimelineDownloadScreenAsPdfButton.vue
+++ components/modules/timeline/TimelineDownloadScreenAsPdfButton.vue
@@ -25,28 +25,29 @@ export default {
       try {
         // eslint-disable-next-line promise/param-names
         const delay = ms => new Promise(res => setTimeout(res, ms))
+        // First page
         this.$emit('switchPage', 0)
-        await delay(500)
+        await delay(300)
         const timelineId = this.timelineId
-        const element = document.getElementById('v-app-root')
-        const doc = new JsPDF('p', 'px', [element.clientHeight / 1.77, element.clientWidth / 1.77])
-        await html2canvas(element).then(function (canvas) {
-          const imgData = canvas.toDataURL('image/png')
-          doc.addImage(imgData, 'PNG', 0, 0)
-        })
+        const linesPage = document.getElementById('v-app-root')
+        const doc = new JsPDF('p', 'pt', [linesPage.offsetWidth, linesPage.offsetHeight])
+        await this.addScreenshot(doc, linesPage)
+        // Second page
         this.$emit('switchPage', 1)
-        await delay(500)
-        const secondElement = document.getElementById('v-app-root')
-        doc.addPage('p', 'px', [secondElement.clientHeight / 1.77, secondElement.clientWidth / 1.77])
-        await html2canvas(secondElement).then(function (canvas) {
-          const imgData = canvas.toDataURL('image/png')
-          doc.addImage(imgData, 'PNG', 0, 0)
-        })
+        await delay(300)
+        const chartsPage = document.getElementById('v-app-root')
+        doc.addPage('p', 'pt', [linesPage.offsetWidth, linesPage.offsetHeight])
+        await this.addScreenshot(doc, chartsPage)
+        this.$emit('switchPage', 0)
         doc.save(`reporte-timeline-${timelineId}.pdf`)
-        await this.$emit('switchPage', 0)
       } catch {
         this.$noty.warn('Ocurrió un error al descargar la timeline')
       }
+    },
+    async addScreenshot (doc, element) {
+      const canvas = await html2canvas(element)
+      const imgData = canvas.toDataURL('image/png')
+      doc.addImage(imgData, 'PNG', 0, 0, doc.internal.pageSize.getWidth(), doc.internal.pageSize.getHeight())
     }
   }
 }
